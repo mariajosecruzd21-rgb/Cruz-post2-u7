7a9de5a67e3e52f9d50546c876de37ba03e020b6
mariajosecruz <02230131003@mail.udes.edu.co>
2026-04-20 17:44:04 -0500

    Creacion del proyecto con ejecucion correcta

---
+++ src/main/java/com/universidad/hexagonal/CruzPost2U7Application.java
@@ -0,0 +1,13 @@
+package com.universidad.hexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CruzPost2U7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CruzPost2U7Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/universidad/hexagonal/CruzPost2U7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.universidad.hexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruzPost2U7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
